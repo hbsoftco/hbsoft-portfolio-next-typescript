896c83635c69a4fc45f19c11833e266257fad885
Hossein <hossein.bajan@gmail.com>
2024-05-18 18:47:11 +0330

    Complete desktop navs

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,20 +2,29 @@ import Link from "next/link";
 
 // Components
 import Nav from "./Nav";
+import { Button } from "./ui/button";
 
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo */}
         <Link href="/">
           <h1 className="text-4xl font-semibold">
             hbsoft<span className="text-accent">.</span>
           </h1>
         </Link>
-        {/* desktop nav */}
-        <div className="hidden">
+
+        {/* desktop nav & hire me button */}
+        <div className="hidden xl:flex items-center gap-8">
           <Nav />
+          <Link href="/contact">
+            <Button>Hire Me</Button>
+          </Link>
         </div>
+
+        {/* Mobile nav */}
+        <div className="xl:hidden"></div>
       </div>
     </header>
   );
