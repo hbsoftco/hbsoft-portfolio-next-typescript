cd4a20734067a59525c3fb8b95dfa0d0b21a3df9
Hossein <hossein.bajan@gmail.com>
2024-05-18 20:19:53 +0330

    Add stair animation when changing pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 // Components
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const jetBrainMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -26,7 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={jetBrainMono.variable}>
         <Header />
-
+        <StairTransition />
         <PageTransition>{children}</PageTransition>
       </body>
     </html>
---
+++ components/Stairs.tsx
@@ -0,0 +1,46 @@
+import { motion } from "framer-motion";
+
+const stairAnimation = {
+  initial: {
+    left: "0%",
+  },
+  animate: {
+    left: "100%",
+  },
+  exit: {
+    left: ["100%", "0%"],
+  },
+};
+
+const reverseIndex = (index: number) => {
+  const totalSteps = 6;
+  return totalSteps - index - 1;
+};
+
+const Stairs = () => {
+  return (
+    <>
+      {/* render 6 animation divs, each representing a step of the stairs */}
+
+      {[...Array(6)].map((_, index) => {
+        return (
+          <motion.div
+            key={index}
+            variants={stairAnimation}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{
+              duration: 0.2,
+              ease: "easeInOut",
+              delay: reverseIndex(index) * 0.1,
+            }}
+            className="h-full w-full bg-white relative"
+          />
+        );
+      })}
+    </>
+  );
+};
+
+export default Stairs;
